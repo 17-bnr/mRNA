eed76bc29c3932d668b2229fc6906244a5e79cc3
17-bnr <k2do0315@gmail.com>
2021-10-19 15:16:00 +0900

    make result

---
+++ Assets/Scripts/DataSender.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataSender{
+    public static string result_text;
+    public static string result_score;
+}
